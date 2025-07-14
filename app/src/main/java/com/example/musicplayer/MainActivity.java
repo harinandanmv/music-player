c91543349dbcddc98776c0bb72eb8af369facfb2
M V Harinandan <harinandanmv11@gmail.com>
2025-07-14 15:27:05 +0530

    Pause Update

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mp.stop();
+                mp.pause();
             }
         });
                 ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
